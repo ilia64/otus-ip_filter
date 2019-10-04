22d5aa686668efd61f923d9c2f7bcb36dab4310d
rj-ilukin <i.lukin@rj-games.com>
2019-10-04 12:34:45 +0300

    change log lib from spdlog to iostream

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+#include <iostream>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    std::cout << "build " << version() << '\n' << "Hello, World!" << std::endl;
 
     return 0;
 }
